37120cdc85be91a7321738c3ccf23fb82e02e71c
Vanillien <ivanpaketov71@gmail.com>
2024-07-13 12:05:33 +0300

    Never gonna give you up

--- Classes/PasteClass.cs
+++ Classes/PasteClass.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 namespace Pastebin1.Classes;
 
 public class Paste
@@ -6,11 +8,77 @@ public class Paste
     private string text;
     private string image;
     private Category category;
-    private float past_expiration;
+    private float paste_expiration;
     private Paste_exposure paste_exposure;
     private int likes;
     private int dislikes;
     private string url;
+
+    public void SetTitle(string title)
+    {
+        this.title = title;
+    }
+
+    public void SetText(string text)
+    {
+        this.text = text;
+    }
+    
+    public void SetImage(string image)
+    {
+        this.image = image;
+    }
+    
+    public void SetCategoryCars()
+    {
+        this.category = Category.cars;
+    }
+    
+    public void SetCategoryCrypt()
+    {
+        this.category = Category.crypt;
+    }
+    
+    public void SetCategoryBottles()
+    {
+        this.category = Category.bottles;
+    }
+    
+    public void SetPaste_expiration(float paste_expiration)
+    {
+        this.paste_expiration = paste_expiration;
+    }
+    
+    public void SetPaste_exposure_public()
+    {
+        this.paste_exposure = Paste_exposure._public;
+    }
+    
+    public void SetPaste_exposure_unlisted()
+    {
+        this.paste_exposure = Paste_exposure._unlisted;
+    }
+    
+    public void SetPaste_exposure_private()
+    {
+        this.paste_exposure = Paste_exposure._private;
+    }
+    
+    public void AddLike()
+    {
+        this.likes++;
+    }
+    
+    public void AddDislike()
+    {
+        this.dislikes++;
+    }
+    
+    public void CreateUrl()
+    {
+        //нужен генератор хэшей, как отдельный сервис
+    }
+    
 }
 
 public enum Category
